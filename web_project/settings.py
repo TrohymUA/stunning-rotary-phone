3f3dc2645791f59cec14c8f0b6adaf317e8c11cc
TrohymUA <19tskalyga@chelsea-academy.org>
2024-11-08 20:16:25 +0000

    dfhndf

--- web_project/settings.py
+++ web_project/settings.py
@@ -71,10 +71,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "web_project.wsgi.application"
 
-STATIC_ROOT = BASE_DIR / 'static_collected'
-
-
-
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
@@ -121,6 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / 'static_collected'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
