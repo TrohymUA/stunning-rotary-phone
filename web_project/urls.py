6efa38f1c726946a1b7cd73c889b47b913a9c57f
TrohymUA <19tskalyga@chelsea-academy.org>
2024-11-08 19:35:38 +0000

    ohbvi

--- web_project/urls.py
+++ web_project/urls.py
@@ -22,7 +22,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path("", include("hello.urls")),
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls) 
 ]
 
 urlpatterns += staticfiles_urlpatterns()
